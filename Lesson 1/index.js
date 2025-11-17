7c2869ac01b2dd0d897665837b95f47b16d015ff
PauloMio <pmcpanopio@gmail.com>
2025-11-17 22:45:55 +0800

    Initial commit

---
+++ Lesson 1/index.js
@@ -0,0 +1,14 @@
+// console.log(`Hello from Paulo`);
+
+// window.alert(`Red Alert!`);
+// window.alert(`Momento Moriiii!!`);
+
+document.getElementById("myH1").textContent = `Hello! from Marcus Aurelius.`;
+document.getElementById("myP").textContent = `Hi! from Seneca.`;
+
+// This is a comment
+
+/*
+    A very
+    long coment
+*/
