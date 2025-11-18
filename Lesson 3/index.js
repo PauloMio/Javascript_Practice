4bf47dc570c6032abc4105b5d5968788a1b349aa
PauloMio <pmcpanopio@gmail.com>
2025-11-18 21:40:19 +0800

    Lesson 3

---
+++ Lesson 3/index.js
@@ -0,0 +1,35 @@
+// arithmetic operators = operands (values, variables, etc), operators (+ - * /)
+// ex. 11 = x + 5;
+
+let students = 30;
+
+// students = students + 13; // adds 13 to the existing number variable when the variable content is loaded. Re-asigning of data with modification.
+// students = students - 5;
+// students = students * 5;
+// students = students / 2;
+// students = students ** 2;
+// let extraStudents = students % 3; // modulo
+
+// students += 1; // shortcut for additions
+// students -= 1;
+// students *= 2;
+// students /= 2;
+// students **= 2;
+// students %= 2;
+
+// students ++;
+// students --;
+
+// console.log(students);
+
+/* 
+Order of Operations 
+1. parentesies
+2. exponents
+3. multiplication & division & modulo
+4. addition & subtraction
+*/
+
+let result = 1 + 2 * 3 + 4 ** 2;
+
+console.log(result);
