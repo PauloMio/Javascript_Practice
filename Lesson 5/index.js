624fc346e1887bd1590e8a78a1b500b43d48dd05
PauloMio <pmcpanopio@gmail.com>
2025-11-21 20:27:51 +0800

    Lesson 5

---
+++ Lesson 5/index.js
@@ -0,0 +1,27 @@
+// type conversion = change the data type of a value to another. Ex. String, Number and Boolean
+
+/*
+let age = window.prompt("How old are you?");
+age = Number(age); // converts the string into Number
+age+=1;
+
+console.log(age, typeof age);
+*/
+
+/*
+let x = "Pasta";
+let y = "Pasta";
+let z = "Pasta";
+*/
+
+let x = "";
+let y = "";
+let z = "";
+
+x = Number(x);
+y = String(y);
+z = Boolean(z); // casting string into a Boolean is a way of finding out if the user input is empty or not.
+
+console.log(x, typeof x);
+console.log(y, typeof y);
+console.log(z, typeof z);
