237d225457869fdacc00c24c9c90c46b7b5d75e7
Paulo Mio <bmothhydra@gmail.com>
2025-12-01 22:48:32 +0800

    Lesson 8

---
+++ Lesson 8/index.js
@@ -0,0 +1,32 @@
+// Math = built-in object that provides a collection of properties and methods
+
+/*
+console.log(Math.PI);   // gives the value of PI which is 3.1415...
+console.log(Math.E);
+*/
+
+let x = 3;
+let y = 2;
+let z = 1;
+
+// z = Math.round(x);   // rounding of number
+// z = Math.floor(x);   // round it down
+// z = Math.ceil(x);    // round it up
+// z = Math.trunc(x);   // eliminate decimals
+// z = Math.pow(x, y);     // raising variable to the power of another variable
+// z = Math.sqrt(x);       // Squareroot function
+// z = Math.log(x);     // Logarithm
+
+/* Trigonometry */
+// z = Math.sin(x);
+// z = Math.cos(x);
+// z = Math.tan(x);
+
+// z = Math.abs(x);    //absolute value
+// z = Math.sign(x);   // sign
+
+// let max = Math.max(x, y, z);   // maximum value
+// let min = Math.max(x, y, z);    // minnimun value
+
+console.log(min);
+
